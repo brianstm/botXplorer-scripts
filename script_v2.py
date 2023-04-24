08f97fd78560496559dced97f266abc1e2a7f971
BriansTjipto <brianstjipto@gmail.com>
2023-04-24 09:51:22 +0800

    new

--- script_v2.py
+++ script_v2.py
@@ -17,22 +17,31 @@ class LidarNav(object):
         self.stop_distance = 0.1 # meters
 
     def scan_callback(self, msg):
+        rospy.logdebug('Received laser scan data')
         # Calculate the minimum distance to an obstacle in front of the robot
         distances = msg.ranges[0:180] # only consider the front half of the scan
         min_distance = min(distances)
 
+        rospy.logdebug('Minimum distance to obstacle: {:.2f} meters'.format(min_distance))
+
         # Adjust the robot's speed based on the distance to the nearest obstacle
         if min_distance <= self.stop_distance:
+            rospy.loginfo('Stopping robot')
             self.cmd_vel_pub.publish(Twist()) # stop the robot
         elif min_distance <= self.slowdown_distance:
-            self.cmd_vel_pub.publish(Twist(linear=rospy.get_param('speed', self.speed) * min_distance / self.slowdown_distance)) # slow down
+            rospy.loginfo('Slowing down robot')
+            speed = rospy.get_param('speed', self.speed) * min_distance / self.slowdown_distance
+            self.cmd_vel_pub.publish(Twist(linear=speed)) # slow down
         else:
-            self.cmd_vel_pub.publish(Twist(linear=rospy.get_param('speed', self.speed))) # full speed
+            rospy.loginfo('Moving robot at full speed')
+            speed = rospy.get_param('speed', self.speed)
+            self.cmd_vel_pub.publish(Twist(linear=speed)) # full speed
 
     def run(self):
         while not rospy.is_shutdown():
             self.rate.sleep()
 
 if __name__ == '__main__':
+    rospy.loginfo('Starting lidar_nav node')
     node = LidarNav()
     node.run()
