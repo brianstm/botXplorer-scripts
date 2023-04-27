0f3f04af2a4cc4d85c2d1eefb3285c394a46c537
BriansTjipto <brianstjipto@gmail.com>
2023-04-27 16:34:45 +0800

    aaa

--- script_calculation.py
+++ script_calculation.py
@@ -4,14 +4,16 @@ import rospy
 from sensor_msgs.msg import LaserScan
 
 def scan_callback(data):
-    ranges = [round(distance, 2) for distance in data.ranges]
-    print("-----")
-    print(len(ranges))
-    for i in ranges:
-        print(i) 
+    state = data.output_count
 
+    field1 = state[0]
+    field2 = state[1]
+
+    print("field1:", field1)
+    print("field2:", field2)
+    print("---------------------------------")
 
 if __name__ == '__main__':
     rospy.init_node('lidar_subscriber')
-    rospy.Subscriber("/scan", LaserScan, scan_callback)
+    rospy.Subscriber("/sick_tim_7xx/lidoutputstate", LaserScan, scan_callback)
     rospy.spin()
