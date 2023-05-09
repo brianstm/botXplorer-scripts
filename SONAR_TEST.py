31b4c2d2043b3183b1b45714b49abfee44a79968
BriansTjipto <brianstjipto@gmail.com>
2023-05-09 17:14:04 +0800

    added sonar test

---
+++ SONAR_TEST.py
@@ -0,0 +1,37 @@
+
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Range
+
+OBSTACLE_DIST = 0.2
+
+class UbiquityBot(object):
+
+    def __init__(self):
+        rospy.init_node('ubiquity_bot')
+
+        self.sonar_sub = rospy.Subscriber('/sonars', Range, self.sonar_callback)
+
+        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+        self.twist_msg = Twist()
+        self.twist_msg.linear.x = 0.2
+
+        self.loop()
+
+    def sonar_callback(self, msg):
+        if msg.range < OBSTACLE_DIST:
+            self.twist_msg.linear.x = 0
+            self.cmd_vel_pub.publish(self.twist_msg)
+
+    def loop(self):
+        rate = rospy.Rate(10) 
+        while not rospy.is_shutdown():
+            self.cmd_vel_pub.publish(self.twist_msg)
+            rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        UbiquityBot()
+    except rospy.ROSInterruptException:
+        pass
---
+++ SONAR_TEST_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Range
+
+def sonar_callback(data):
+    if data.range < 0.5: 
+        twist_cmd.linear.x = 0.0
+    else:
+        twist_cmd.linear.x = 0.7
+
+rospy.init_node('sonar_controller')
+rospy.Subscriber('/sonars', Range, sonar_callback)
+
+twist_cmd = Twist()
+twist_cmd.linear.x = 0.7
+twist_cmd.linear.y = 0.0
+twist_cmd.linear.z = 0.0
+twist_cmd.angular.x = 0.0
+twist_cmd.angular.y = 0.0
+twist_cmd.angular.z = 0.0
+
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+rate = rospy.Rate(10)
+
+while not rospy.is_shutdown():
+    pub.publish(twist_cmd)
+    rate.sleep()
