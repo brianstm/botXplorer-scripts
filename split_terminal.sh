9feb33e9da30b87859a6dfbaf1620d1889ef01b8
BriansTjipto <brianstjipto@gmail.com>
2023-07-31 14:19:29 +0800

    split terminal change

--- split_terminal.sh
+++ split_terminal.sh
@@ -4,11 +4,10 @@
 tmux new-session -d -s split_terminal
 
 # Split the window into 4 panes
+tmux split-window -h -p 50
 tmux split-window -v -p 50
 tmux select-pane -t 0
-tmux split-window -h -p 50
-tmux select-pane -t 2
-tmux split-window -h -p 50
+tmux split-window -v -p 50
 
 # Move to the top-left pane and split it horizontally
 tmux select-pane -t 0
@@ -22,4 +21,3 @@ tmux swap-pane -L
 
 # Attach to the tmux session
 tmux attach-session -t split_terminal
-
