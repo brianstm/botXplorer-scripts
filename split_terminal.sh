c2cdd5242188a648006224d2eccab567dfb2236d
spsickamr <spsickbotxplorer@gmail.com>
2023-07-31 13:50:58 +0800

    comments

--- split_terminal.sh
+++ split_terminal.sh
@@ -20,3 +20,17 @@ tmux select-layout even-vertical
 
 # Attach to the new session (you will see the four equal panes)
 tmux attach-session -t split-screen
+
+
+# sick@sick:~$ cd /home/sick/Documents/
+# sick@sick:~/Documents$ chmod +x split_terminal.sh 
+# sick@sick:~/Documents$ ls -l
+# total 4
+# -rwxrwxrwx 1 sick sick 565 Jul 31 13:46 split_terminal.sh
+# sick@sick:~/Documents$ ./split_terminal.sh 
+# bash: ./split_terminal.sh: /usr/bin/bash^M: bad interpreter: No such file or directory
+# sick@sick:~/Documents$ bash ./split_terminal.sh 
+# ./split_terminal.sh: line 2: $'\r': command not found
+# ./split_terminal.sh: line 23: syntax error: unexpected end of file
+# sick@sick:~/Documents$ 
+
