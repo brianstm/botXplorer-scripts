b77150ab8df6e588d80fe58f17470ff8b1b6a31c
spsickamr <spsickbotxplorer@gmail.com>
2023-08-01 11:40:58 +0800

    commit

--- terminal_split.sh
+++ terminal_split.sh
@@ -30,3 +30,11 @@ xdotool type "bx"
 xdotool key Return
 
 xdotool windowactivate --sync "${window_ids[1]}"
+
+[ WARN] [1690861195.156498128]: Control loop missed its desired rate of 5.0000Hz... the loop actually took 0.2026 seconds
+[ WARN] [1690861195.346218475]: Map update loop missed its desired rate of 5.0000Hz... the loop actually took 0.2021 seconds
+[ WARN] [1690861195.582957311]: Control loop missed its desired rate of 5.0000Hz... the loop actually took 0.2291 seconds
+[ WARN] [1690861195.880729960]: TebLocalPlannerROS: trajectory is not feasible. Resetting planner...
+[ WARN] [1690861201.196500579]: Map update loop missed its desired rate of 1.0000Hz... the loop actually took 4.8882 seconds
+[ WARN] [1690861201.354069182]: Clearing both costmaps outside a square (3.00m) large centered on the robot.
+[ WARN] [1690861204.588422646]: Map update loop missed its desired rate of 1.0000Hz... the loop actually took 2.3919 seconds
